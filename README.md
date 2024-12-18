a1ca7264324179f9636700d94cdc0001aec1e869
danilowlc <danilowlc@gmail.com>
2024-12-18 12:17:51 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do site www.tabnews.com.br para treinamento de programação curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Estou apredendo a criar projetos em node/next</h1>
+function Home() {
+  return <h1>Estou apredendo a criar projetos em node/next</h1>;
 }
 
 export default Home;
