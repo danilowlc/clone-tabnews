7d1ca2e92d04495c3d61b8fc3a293ea577a2522f
danilowlc <danilowlc@gmail.com>
2024-12-23 10:06:08 -0300

    add `/api/v1/status` endpoint

--- models/calculadora.js
+++
@@ -1,7 +0,0 @@
-//arquivo temporario para didatica
-
-function somar(a, b) {
-    return a + b;
-}
-
-exports.somar = somar;
--- tests/calculadora.test.js
+++
@@ -1,5 +0,0 @@
-const calculadora = require("../models/calculadora.js");
-
-test("somar 1 + 2 = 3", () => {
-  expect(calculadora.somar(1, 2)).toBe(3);
-});
