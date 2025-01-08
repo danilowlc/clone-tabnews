b5c0815bb32ec4edc70ac2d63de4ff89274da329
danilowlc <danilowlc@gmail.com>
2025-01-08 11:26:36 -0300

    alterando a pagina home

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Estou apredendo a criar projetos em node/next (Homologação)</h1>;
+  return <h1>Estou apredendo a criar projetos em node/next</h1>;
 }
 
 export default Home;
