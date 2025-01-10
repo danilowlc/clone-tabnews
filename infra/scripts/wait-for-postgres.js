8f59c842a94b1bd246565d635fe961271ef48076
danilowlc <danilowlc@gmail.com>
2025-01-10 10:29:28 -0300

    modifed change connection waiting animation in `wait-for-postegres.js`

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -1,12 +1,19 @@
 const {exec} = require("node:child_process");
 
+let contador = 0; // Contador global para o relógio
+let relogio = ["|", "/", "–", "\\"]; // Símbolos do relógio
+
 function checkPostgres() {
   exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
 
   function handleReturn(error, stdout) {
     if (stdout.search("accepting connections") === -1) {
-      process.stdout.write(".");
-      checkPostgres();
+      // Move o cursor para o início da linha e exibe o relógio junto com a mensagem
+      process.stdout.write(`\r${relogio[contador % relogio.length]} ⌛ Aguardando PostgreSQL aceitar conexões...`);
+      contador++;
+
+      // Chama a função novamente após 250ms
+      setTimeout(checkPostgres, 250);
       return;
     }
     console.log("\n🟢 Postgres está pronto!\n");
